4325c061b2741f87d929d50d9047495fd933a4bb
Moamen <Moamenbekheet98@gmail.com>
2022-08-24 17:34:41 +0200

    added a script and it is bugged

--- script.js
+++ script.js
@@ -11,8 +11,23 @@ Book.prototype.info = function(){
     return `${this.title} by ${this.author}, ${this.num_pages} pages, ${this.read ? 'Already read':'not yet read'}`
 }
 
+let title, author, num_pages, read;
+
 const addBtn = document.querySelector('#add-book');
-const overlay = document.querySelector('.overlay');
-addBtn.addEventListener('click',function(){
-    overlay.style.dislay
+const overlay = document.querySelector(".overlay");
+const overlayForm = document.querySelector('.child-overlay');
+const form = document.querySelector('form');
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookNumPages = document.querySelector('#num_pages');
+const readAlready = document.querySelector('#read');
+
+addBtn.addEventListener('click',()=>{overlay.style.display = 'flex'})
+
+overlayForm.addEventListener('click', function(){
+    overlay.style.display = 'none';
+});
+form.addEventListener('submit', ()=>{
+    myLibrary.push(new Book(bookTitle.value, bookAuthor.value, bookNumPages.value, readAlready.value))
+    document.createElement('div')
 })

c17c09ee43c56e03034543706856c883a572ce42
Moamen <Moamenbekheet98@gmail.com>
2022-08-25 08:42:20 +0200

    added a FormData() object to extract data from the book adding form and display a book card on screen

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ function Book(title, author, num_pages, read){
     this.title = title;
     this.author = author;
     this.num_pages = num_pages;
-    this.read = read;
+    this.read = (read==='on')? 'Already read':'Not yet read';
 }
 
-Book.prototype.info = function(){
-    return `${this.title} by ${this.author}, ${this.num_pages} pages, ${this.read ? 'Already read':'not yet read'}`
-}
-
-let title, author, num_pages, read;
+// Book.prototype.info = function(){
+//     return `${this.title} by ${this.author}, ${this.num_pages} pages, ${this.read ? 'Already read':'not yet read'}`
+// }
+let bookCount = 0;
 
 const addBtn = document.querySelector('#add-book');
 const overlay = document.querySelector(".overlay");
@@ -21,13 +20,76 @@ const bookTitle = document.querySelector('#title');
 const bookAuthor = document.querySelector('#author');
 const bookNumPages = document.querySelector('#num_pages');
 const readAlready = document.querySelector('#read');
-
+const bookCard = document.createElement('div');
 addBtn.addEventListener('click',()=>{overlay.style.display = 'flex'})
 
 overlayForm.addEventListener('click', function(){
     overlay.style.display = 'none';
 });
-form.addEventListener('submit', ()=>{
-    myLibrary.push(new Book(bookTitle.value, bookAuthor.value, bookNumPages.value, readAlready.value))
-    document.createElement('div')
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let getFormData = new FormData(form);
+    let book = new Book(getFormData.get('title'), getFormData.get('author'), getFormData.get('num_pages'), readAlready.value);
+    myLibrary.push(book);
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-item-card');
+    const bookDetails = document.createElement('div')
+    bookCard.classList.add('book-details');
+    bookCard.appendChild(bookDetails);
+
+    const flexIcons = document.createElement('div');
+    flexIcons.classList.add('flex-icons');
+
+    const del = document.createElement('div')
+    del.classList.add('delete');
+    const delLabel = document.createElement('label');
+    delLabel.for = 'del-button'
+    del.appendChild(delLabel);
+    delLabel.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>';
+    const delButton = document.createElement('button');
+    delButton.textContent = 'remove';
+    delButton.type = 'button';
+    delButton.id = 'del-button';
+    del.appendChild(delButton);
+    flexIcons.appendChild(del);
+
+    const markAsRead = document.createElement('div')
+    markAsRead.classList.add('mark-as-read');
+    const markAsReadLabel = document.createElement('label');
+    markAsReadLabel.for = 'read-button'
+    markAsRead.appendChild(markAsReadLabel);
+    markAsReadLabel.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z" /></svg>';
+    const markAsReadButton = document.createElement('button');
+    markAsReadButton.textContent = 'mark as read'
+    markAsReadButton.type = 'button';
+    markAsReadButton.id = 'read-button';
+    markAsRead.appendChild(markAsReadButton);
+    flexIcons.appendChild(markAsRead);
+
+    bookCard.appendChild(flexIcons)
+
+    const books = document.querySelector('.books')
+
+    let i = 0;
+    for(let prop in book){
+        if(i==0){
+            const header = document.createElement('h2');
+            header.textContent = book[prop];
+            bookDetails.appendChild(header);
+            i++
+            continue 
+            //the title should be an h2 element the rest are paragraphs
+        }
+        const paragraph = document.createElement('p');
+        paragraph.textContent = book[prop];
+        bookDetails.appendChild(paragraph);
+    }
+
+    books.appendChild(bookCard);
+    book.id = bookCount;
+    bookCount++;
+    bookTitle.value='';
+    bookAuthor.value='';
+    bookNumPages.value='';
+    overlay.style.display = 'none';
 })
